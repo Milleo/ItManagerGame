728213206e61a0b30b53fc79095fd5f77f6a780d
Rafael Milléo <rafael@milleo.net>
2023-07-27 17:09:45 -0300

    Adding prisma to the project

---
+++ backend/prisma/migrations/20230727200557_users_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` CHAR(36) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` CHAR(72) NOT NULL,
+    `banned` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
